960822c05f887443128e0fb278b14ee3772c44ba
Helge Müller <hmu@informatik.uni-kiel.de>
2019-01-29 10:15:41 +0100

    Initial commit

--- addon/controllers/tutorial.js
+++ addon/controllers/tutorial.js
@@ -1,7 +1,26 @@
 import Controller from '@ember/controller';
+import { inject as service } from "@ember/service";
+import { task } from 'ember-concurrency';
 
 export default Controller.extend({
+  store: service(),
 
+  tutorials: null,
+
+  init() {
+      this._super(...arguments);
+      this.updateTutorialList(true);
+    },
+  updateTutorialList(reload) {
+    this.set('tutorials', []);
+    this.get('store').findAll('tutorial', { reload })
+      .then(tutorials => {
+        let tutorialList = tutorials.toArray();
+        // sort by id
+        userList.sort((tutorial1, tutorial2) => parseInt(tutorial1.id) < parseInt(tutorial2.id) ? -1 : 1);
+        this.set('tutorials', userList);
+      });
+  },
 
 actions: {
   // body
