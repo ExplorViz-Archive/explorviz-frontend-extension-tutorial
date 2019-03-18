37fe1edf4475efa88b0471dafc8b41468e8045a2
Helge Müller <hmu@informatik.uni-kiel.de>
2019-03-18 16:49:23 +0100

    added serialized landscapes

--- addon/models/tutorial.js
+++ addon/models/tutorial.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   text: DS.attr('string'),
+  landscape: DS.attr('string'),
   sequences: DS.hasMany('sequence'),
 });
