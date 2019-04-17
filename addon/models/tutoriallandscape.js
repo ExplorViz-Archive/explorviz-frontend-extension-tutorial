d9ec632700c0ed9961426e6bc913a074a5daff1b
Helge Müller <hmu@informatik.uni-kiel.de>
2019-04-17 18:03:11 +0200

    model changes

--- addon/models/tutoriallandscape.js
+++ addon/models/tutoriallandscape.js
@@ -1,7 +1,6 @@
 import DS from 'ember-data';
+import Landscape from "explorviz-frontend/model/landscape"
 
-export default DS.Model.extend({
-  timestamp: DS.attr('String'),
-  //landscape: DS.attr('String'),
-  landscape: DS.belongsTo('landscape',{inverse: null}),
+export default Landscape.extend({
+  timestamp: DS.belongsTo('tutorialtimestamp'),
 });
---
+++ addon/models/tutorialtimestamp.js
@@ -0,0 +1,4 @@
+import Timestamp from "explorviz-frontend/model/timestamp"
+export default Timestamp.extend({
+    name: DS.attr('String')
+});
